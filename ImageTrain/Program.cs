57e48f01f8dd379b0ba693533ef9fe99f4b79346
jiasheng luo <errorgz@qq.com>
2023-05-28 06:36:37 +0800

    2023

--- ImageTrain/Program.cs
+++ ImageTrain/Program.cs
@@ -1,9 +1,14 @@
-﻿using System.Diagnostics;
+﻿using OpenCvSharp;
+using OpenCvSharp.ML;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.Linq;
 using TorchSharp;
 using TorchSharp.Modules;
 using static TorchSharp.torch;
 using static TorchSharp.torch.nn;
 using static TorchSharp.torch.nn.functional;
+using static TorchSharp.torch.utils;
 using static TorchSharp.torch.utils.data;
 
 namespace ImageTrain
@@ -12,196 +17,35 @@ namespace ImageTrain
     {
         static void Main(string[] args)
         {
-
-            var tensorboard = torch.utils.tensorboard.SummaryWriter();
-
-            Console.WriteLine($"新建记录文件夹：{tensorboard.LogDir}");
-
-
-            YAMLdatabase db = new YAMLdatabase(@"DataSet\animals.v2-release.yolov5pytorch");
-            db.Save("config.yaml");
-            //YAMLdatabase db2 = new YAMLdatabase();
-            //db2.Load("config.yaml");
+            var DataPath = @"DataSet\animals.v2-release.yolov5pytorch";
+            var configFilePath = Path.Combine(DataPath, "config.yaml");
+            YAMLdatabase db = new YAMLdatabase(DataPath);
+            db.Save(configFilePath);
+            //Train.DoTrain(db);
 
 
-            var device = torch.cuda.is_available() ? torch.CUDA : torch.CPU;
-            var model = ImageTrain.torchvision.models.resnet34(num_classes: db.Labels.Count, device: device);
+            db = YAMLdatabase.LoadYAML(configFilePath);
+            Detect det = new Detect(db);
 
-
-
-            if (File.Exists("best.pt"))
+            
+            Stopwatch s = new Stopwatch();
+            foreach (var file in db.ValidImage)
             {
-                Console.WriteLine("加载best.pt权重文件");
-                model.load("best.pt");
+                s.Restart();
+                var labels=det.GetDetectLabel(file.Value.ImageFile);
+                s.Stop();          
+
+                var msg实际类型 = string.Join(", ", file.Value.bbox.Select(o => o.Item1));
+                string msg检测类型 = string.Join(", ", labels.Select(label => $"{label.Item1}:{label.Item3}({label.Item2:F4})"));
+
+
+                Console.WriteLine($"{file.Value.ImageFile} {s.ElapsedMilliseconds}ms \t Detect:{msg检测类型} Actual:{msg实际类型}" );
+                
             }
 
-            using YOLOv5_Dataset train_data = new YOLOv5_Dataset(db.TrainImage, db.Labels.Count);
-            using YOLOv5_Dataset test_data = new YOLOv5_Dataset(db.ValidImage, db.Labels.Count);
-            using var train = new DataLoader(train_data, db._trainBatchSize, device: device, num_worker: 4, shuffle: true);
-            using var test = new DataLoader(test_data, db._testBatchSize, device: device, num_worker: 4, shuffle: false);
-
-            using (var optimizer = torch.optim.Adam(model.parameters(), 0.001))
-            {
-                var bestAccuracy = 0.0;
-                var bestModelPath = "best.pt";
-                var lastModelPath = "last.pt";
-
-                Stopwatch totalSW = new Stopwatch();
-                totalSW.Start();
-
-                for (var epoch = 1; epoch <= db._epochs; epoch++)
-                {
-                    Stopwatch epchSW = new Stopwatch();
-                    epchSW.Start();
-
-
-                    StaticLib.Log($"epoch: {epoch}...");
-                    var accuracy_train = TrainOrTest(model, optimizer, train, tensorboard, epoch, true);
-
-                    if (epoch % 10 == 0)
-                    {
-                        var accuracy_test = TrainOrTest(model, optimizer, test, tensorboard, epoch, false);
-                    }
-
-                    // Save the model at each epoch
-                    model.save(lastModelPath);
-
-                    StaticLib.Log($"Save Last Model:{lastModelPath}");
-                    if (accuracy_train > bestAccuracy)
-                    {
-                        bestAccuracy = accuracy_train;
-                        // Save the best model                        
-                        model.save(bestModelPath);
-
-                        StaticLib.Log($"Save Best Model:{bestModelPath}");
-                    }
-
-                    epchSW.Stop();
-                    StaticLib.Log($"Elapsed time for this epoch: {epchSW.Elapsed.TotalSeconds} s.");
-
-                    //if (totalSW.Elapsed.TotalSeconds > db._timeout) break;
-                }
-
-                totalSW.Stop();
-                StaticLib.Log($"Elapsed training time: {totalSW.Elapsed} s.");
-            }
-
-
-            model.Dispose();
 
         }
-
-
-        private static double TrainOrTest(
-            Module<torch.Tensor, torch.Tensor> model,
-            torch.optim.Optimizer optimizer,
-            DataLoader dataLoader,
-            SummaryWriter tensorboard,
-            int epoch,
-            bool isTraining)
-        {
-            if (isTraining)
-            {
-                model.train();
-            }
-            else
-            {
-                model.eval();
-            }
-
-            long total = 0;
-            double total_loss = 0.0f;
-            double total_correct = 0.0f;
-            double accuracy = 0.0f;
-
-            string mode = isTraining ? "Train" : "Test";
-
-            using (var d = torch.NewDisposeScope())
-            {
-                foreach (var data in dataLoader)
-                {
-                    optimizer.zero_grad();
-
-                    var target = data["label"];
-                    var image = data["data"];
-
-                    var prediction = model.call(image);
-                    var lsm = log_softmax(prediction, 1);
-
-                    var output = cross_entropy(lsm, target);
-
-                    if (isTraining)
-                    {
-                        output.backward();
-                        optimizer.step();
-                    }
-                    var batch_count = target.shape[0];
-                    var sample_count = target.shape[1];
-
-                    total += batch_count;
-
-                    var batch_loss = output.ToSingle();
-                    total_loss += batch_loss;
-
-                    double batch_correct = 0;
-                    List<(int, int)> pt_data = new List<(int, int)>();              
-
-                    for(int b=0;b<batch_count;b++) 
-                    {
-                        var batch_prediction = prediction[b];
-                        var batch_target = target[b];
-                        for (int i = 0; i < sample_count; i++)
-                        {
-                            var sample_prediction = batch_prediction[i];
-                            var sample_target = batch_target[i];
-
-                            (int, int) pt = (0, 0);
-
-                            pt.Item1 = sample_prediction.ToSingle() > 0.5 ? 1 : 0;
-                            pt.Item2 = sample_target.ToSingle() == 1 ? 1 : 0;
-                            pt_data.Add(pt);
-                            if (pt.Item1 == pt.Item2)
-                                batch_correct += 1;
-
-                        }
-                    }
-
                  
-
-                    total_correct += batch_correct/sample_count;
-                    var pt_msg = string.Join(",", pt_data);
-                    //StaticLib.Log($"[{mode}] batch_data:{pt_msg}");
-                    StaticLib.Log($"batch_loss:{batch_loss}/{batch_count*sample_count}\t batch_correct:{batch_correct}/{batch_count*sample_count}");
-
-                    d.DisposeEverything();
-                }
-            }
-
-            if (total == 0)
-            {
-                var msg = "DataSet.Count()==0";
-                StaticLib.Log(msg);
-                throw new Exception(msg);
-            }
-            accuracy = total_correct / total;
-            var avgLoss = total_loss / total;
-
-            StaticLib.Log($"\r{mode}: Average loss {avgLoss.ToString("0.0000")} | Accuracy {accuracy.ToString("0.0000")}");
-            if (isTraining)
-            {
-                tensorboard.add_scalar("accuracy", (float)accuracy, epoch);
-                tensorboard.add_scalar("lost", (float)avgLoss, epoch);
-            }
-            else
-            {
-                tensorboard.add_scalar("val_accuracy", (float)accuracy, epoch);
-                tensorboard.add_scalar("val_lost", (float)avgLoss, epoch);
-            }
-
-            return accuracy;
-
-        }
-
 
     }
 
